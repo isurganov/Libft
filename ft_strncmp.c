780efcd86189cffd18bc468894e15dae780b4894
isurganov <devsurganov@gmail.com>
2021-10-09 17:38:04 +0300

    update homework

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spzona <spzona@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 17:24:14 by spzona            #+#    #+#             */
+/*   Updated: 2021/10/09 17:24:50 by spzona           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int		i;
 
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spzona <spzona@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 17:24:20 by spzona            #+#    #+#             */
+/*   Updated: 2021/10/09 17:24:35 by spzona           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t i;
-	size_t j;
-	size_t x;
+	size_t	i;
+	size_t	j;
+	size_t	x;
 
 	i = 0;
 	if (needle[0] == '\0')
