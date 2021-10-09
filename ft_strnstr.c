bdcf7f0ad43796a9f4e269723f247eafbf90aa8a
iasurganov <devsurganov@gmail.com>
2021-10-09 14:52:36 +0300

    Homework funcs

---
+++ ft_strnstr.c
@@ -0,0 +1,31 @@
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t j;
+	size_t x;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+			x = i;
+		while (haystack[i] == needle[j] && haystack[i] != '\0' && len > i)
+		{
+			i++;
+			j++;
+		}
+		if (j == ft_strlen(needle))
+			return ((char *)haystack + x);
+		if (j > 0)
+			i = x + 1;
+		else
+			i++;
+	}
+	return (NULL);
+}
